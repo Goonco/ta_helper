f84fb682a120855dfcda3c6279dd579119061027
Goonco <s07019@naver.com>
2024-04-03 12:44:49 +0900

    :sparkles: Phase #7 구현 완료 * 동명이인을 고려하여 학번 채으로 로직 변경

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from utils.file_oraganizer import File_Organizer
+from utils.user_input import User_Input
+from utils.file_organizer import File_Organizer
 from utils.evaluator import Evaluator
 from utils.result_printer import Result_Printer
 
 def main() :
-    names = File_Organizer.run()
-    eval_result = Evaluator(names).run();
+    inputs = User_Input()
+    ids = File_Organizer(inputs.delete_file_flag, inputs.total_problem, inputs.target_problem).run()
+    eval_result = Evaluator(ids, inputs.ignore_space_flag, inputs.essentials, inputs.forbiddens).run();
     Result_Printer.run(eval_result);
 
 main()
